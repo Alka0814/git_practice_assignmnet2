98e2437b3813abca60587d224086e92a58ac762b
unknown <alkatrivedi0814@gmail.com>
2022-12-10 22:43:29 +0530

    done

---
+++ palindrom_prime.js
@@ -0,0 +1,30 @@
+let str=naman;
+let rev=""
+for(i=str.length-1;i>=0;i--){
+rev+=str[i];
+}
+if(rev==str){
+console.log("yes")
+}else{
+console.log("No")
+}
+}
+
+
+
+let count=0;
+for(i=0;i<=num;i++){
+if(num%i==0){
+count++;
+}
+}
+if(count==2){
+console.log(i+"is prime);
+}else{
+console.log(i+"is not prime")
+}
+
+
+
+
+
